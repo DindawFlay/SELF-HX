9e1ff59a8f59cf0277c005279c79253103cc66ff
DindawFly <97446180+DindawFlay@users.noreply.github.com>
2022-04-13 08:56:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -55,9 +55,9 @@ const { jadibot, stopjadibot, listjadibot } = require('./lib/jadibot')
 
 banChats = true
 offline = false
-targetpc = '6285751056816'
-owner = '6285751056816'
-fake = 'HEXAGONZ'
+targetpc = '6289515233398'
+owner = '6289515233398'
+fake = 'NbillaNbl'
 numbernye = '0'
 waktu = '-'
 alasan = '-'
